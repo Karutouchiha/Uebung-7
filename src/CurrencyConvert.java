ac9e79f725fa22384d26e4f2be551cb02c0ce272
Sarah Haboeck <sarah.haboeck@htl-wels.at>
2020-11-12 12:56:36 +0100

    3 Convert

---
+++ src/CurrencyConvert.java
@@ -0,0 +1,19 @@
+public class CurrencyConvert {
+    private int toConvert;
+    //1Euro = 124Yen
+
+    public Double toEuro(Double Yen) {
+        double Euro;
+        Euro = Yen / 124;
+        return Euro;
+
+    }
+
+    public double toYen(double Euro){
+        double Yen;
+
+        Yen = Euro * 124;
+
+        return Yen;
+    }
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+
 public class Controller {
+
+    @FXML private Button b_convert;
+    @FXML private TextField txt_Euro;
+    @FXML private TextField txt_Yen;
+
+    @FXML
+    public void convert(){
+
+    }
 }
